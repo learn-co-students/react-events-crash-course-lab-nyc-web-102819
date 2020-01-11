e3a2b149bff375ab79cf433ee30a01882ec13acd
eliskim27 <eliskim27@gmail.com>
2020-01-11 10:42:05 -0500

    Done.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -8,17 +8,11 @@ export default class ChromeBoisDomain extends Component {
     return (drawChromeBoiAtCoords(e.clientX, e.clientY))
   }
   
-   
-  /* TODO: Add an event listener to the `<canvas>` element to capture when a key
-  /* is pressed. When a key is pressed, an event handler should invoke the the
-  /* provided `resize` function with a single argument of either '+' or '-'
-  /* if the key pressed was 'a', then it should call `resize` with '+'
-  /* if the key pressed was 's', then it should call `resize` with '-' 
-   */
+
   handleKeyPress = (e) => {
     if (e.key === 'a') {
       return resize('+')
-    } else {
+    } else if (e.key === 's') {
       return resize('-')
     }
   }
